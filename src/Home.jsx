791378be625dbe7109192824e6d76ab79dd70b88
Sushil Dangi <dangi.sushil5@gmail.com>
2023-07-28 15:33:53 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import IntroPost from "./components/IntroPost";
+import Blogs from "./components/Blogs";
+import Footer from "./components/Footer";
+
+const Home = () => {
+  return (
+    <dev>
+      <Header />
+      <Search />
+      <IntroPost />
+      <Blogs />
+      <Footer />
+    </dev>
+  );
+};
+
+export default Home;
---
+++ src/components/IntroPost.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IntroPost = () => {
+  return (
+    <div>IntroPost</div>
+  )
+}
+
+export default IntroPost
