6da5a9f6ca600e0960caa36a276e977673f15e1c
Sushil Dangi <dangi.sushil5@gmail.com>
2023-07-30 22:27:08 +0530

    Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import BlogDetails from "./pages/BlogDetails";
 
 function App() {
   return (
     <div>
-       <Home />
+      <Routes>
+        <Route path="/" element={  <Home />}/>
+        <Route path="/blog-details" element={<BlogDetails />}/>
+      </Routes>
     </div>
   );
 }
